9b50676b21e25e68868a301fb569d12715cae012
lukix <lukix.95@gmail.com>
2017-10-10 21:38:24 +0200

    Add activationNode class

---
+++ recurrent-neural-nets/ActivationNode.cpp
@@ -0,0 +1,51 @@
+#include "stdafx.h"
+#include "ActivationNode.h"
+
+
+ActivationNode::ActivationNode(double (*activationFunc)(double), double(*activationFuncDerivative)(double)) {
+	this->activationFunc = activationFunc;
+	this->activationFuncDerivative = activationFuncDerivative;
+}
+
+ActivationNode::~ActivationNode() {
+}
+
+double ActivationNode::sigmoid(double x) {
+	double beta = 1.0;
+	return 1.0 / (1.0 + exp(-beta * x));
+}
+
+double ActivationNode::sigmoidDerivative(double x) {
+	double beta = 1.0;
+	double f = 1.0 / (1.0 + exp(-beta * x));
+	return f * (1.0 - f);
+}
+
+double ActivationNode::hyperbolicTangent(double x) {
+	return sinh(x) / cosh(x);
+}
+
+double ActivationNode::hyperbolicTangentDerivative(double x) {
+	return 1.0 - pow(sinh(x) / cosh(x), 2);
+}
+
+std::vector<std::vector<double>> ActivationNode::processInputs() {
+	std::vector<std::vector<double>> result;
+	result.push_back(std::vector<double>());
+	result[0].reserve(this->inputVectors[0].size());
+	for (int i = 0; i < this->inputVectors[0].size(); i++) {
+		result[0].push_back(this->activationFunc(this->inputVectors[0][i]));
+	}
+	return result;
+}
+
+std::vector<std::vector<double>> ActivationNode::processOutputErrors() {
+	std::vector<std::vector<double>> result;
+	result.push_back(std::vector<double>());
+	result[0].reserve(this->inputVectors[0].size());
+	std::vector<double> lastInputVector = this->prevNodes[0]->getLastOutput()[0];
+	for (int i = 0; i < this->inputVectors[0].size(); i++) {
+		result[0].push_back(this->inputVectors[0][i] * this->activationFuncDerivative(lastInputVector[i]));
+	}
+	return result;
+}
---
+++ recurrent-neural-nets/ActivationNode.h
@@ -0,0 +1,18 @@
+#pragma once
+#include "Node.h"
+class ActivationNode :
+	public Node {
+public:
+	ActivationNode(double (*activationFunc)(double), double(*activationFuncDerivative)(double));
+	~ActivationNode();
+	static double sigmoid(double x);
+	static double sigmoidDerivative(double x);
+	static double hyperbolicTangent(double x);
+	static double hyperbolicTangentDerivative(double x);
+protected:
+	virtual std::vector<std::vector<double>> processInputs();
+	virtual std::vector<std::vector<double>> processOutputErrors();
+	double(*activationFunc)(double);
+	double(*activationFuncDerivative)(double);
+};
+
